cd98e044de0f4cbd32d7208a243b46fb45e063ac
Tim van der Leeuw <tim@l1nda.nl>
2018-06-06 21:06:58 +0200

    Add some images supposedly portrait mode; although for unknown reasons most of them display in landsacpe

--- public/js/birthday.js
+++ public/js/birthday.js
@@ -1,22 +1,51 @@
+function pickImage() {
+    var imgList =  (window.innerWidth < window.innerHeight) ? imagesPortrait : imagesLandscape;
+    var randomNumber = Math.floor(Math.random() * imgList.length);
+    return 'url(' + imgList[randomNumber] + ')';
+}
+
+var imagesLandscape=[
+    'images/backgrounds/IMG_0369.jpg',
+    'images/backgrounds/IMG_0622.jpg',
+    'images/backgrounds/IMG_0878.jpg',
+    'images/backgrounds/IMG_0879.jpg',
+    'images/backgrounds/IMG_0884.jpg',
+    'images/backgrounds/IMG_0892.jpg',
+    'images/backgrounds/IMG_6010_mod.jpg',
+    'images/backgrounds/IMG_6023.jpg',
+    'images/backgrounds/IMG_20150721_073551.jpg',
+    'images/backgrounds/IMG_20150721_073630.jpg',
+    'images/backgrounds/IMG_20150729_074506.jpg',
+    'images/backgrounds/IMG_20150729_093540.jpg',
+    'images/backgrounds/IMG_20150729_105555.jpg',
+    'images/backgrounds/IMG_20150729_115306.jpg',
+    'images/backgrounds/IMG_20150729_125913.jpg',
+    'images/backgrounds/IMG_20150731_101316.jpg',
+    // 'images/backgrounds/IMG_20160427_095902.jpg',
+    // 'images/backgrounds/IMG_6136.jpg',
+    // 'images/backgrounds/IMG_6182.jpg',
+];
+var imagesPortrait = [
+    'images/backgrounds/DSC_0303.jpg',
+    'images/backgrounds/IMG_0018.jpg',
+    'images/backgrounds/IMG_0502.jpg',
+    'images/backgrounds/IMG_2154.jpg',
+    'images/backgrounds/IMG_3743.jpg',
+    'images/backgrounds/IMG_4375.jpg',
+    'images/backgrounds/IMG_4415.jpg',
+    'images/backgrounds/IMG_20160911_134950.jpg',
+];
+
+function setBackgroundImage(bgImg) {
+    $('body').css({'background': bgImg, 'background-size': 'cover'});
+}
+
+function onWindowResize() {
+    setBackgroundImage(pickImage());
+}
 
 $(document).ready(function(){
-    var imagesLandscape=[
-        'images/backgrounds/IMG_0369.jpg',
-        'images/backgrounds/IMG_0622.jpg',
-        'images/backgrounds/IMG_0878.jpg',
-        'images/backgrounds/IMG_0879.jpg',
-        'images/backgrounds/IMG_0884.jpg',
-        'images/backgrounds/IMG_0892.jpg',
-        'images/backgrounds/IMG_6010_mod.jpg',
-        'images/backgrounds/IMG_6023.jpg',
-        'images/backgrounds/IMG_6136.jpg',
-        'images/backgrounds/IMG_6182.jpg',
-    ];
-
-    var randomNumber = Math.floor(Math.random() * imagesLandscape.length);
-    var bgImg = 'url(' + imagesLandscape[randomNumber] + ')';
-
-    // TODO: This doesn't work for all aspect ratios. Have portrait images for portrait window sizes.
-    $('body').css({'background':bgImg, 'background-size': 'cover' });
+    onWindowResize();
+    window.onresize = onWindowResize;
 
 });
